1639ad08b5d0093bdcb77d60de356ba34c08a34d
RedDed1 <jonatha.marvie@gmail.com>
2021-06-11 09:55:03 -0300

    atualização

--- uploadImagens/formulario.php
+++ uploadImagens/formulario.php
@@ -29,9 +29,16 @@
  <?php
  if(isset($_FILES['pic']))
  {
+    $testName = $_POST['imgName'];
     $ext = strtolower(substr($_FILES['pic']['name'],-4)); //Pegando extensão do arquivo
-    $new_name = $_POST['imgName'] . $ext; //Definindo um novo nome para o arquivo
-    $dir = './imagens/'; //Diretório para uploads
+    if ($testName =! "") {
+      $new_name = date("Y.m.d-H.i.s") . $ext; //Definindo um novo nome para o arquivo
+      $dir = './imagens/'; //Diretório para uploads
+    } else if ($testName == ""){
+      $new_name = date("Y.m.d-H.i.s") . $ext; //Definindo um novo nome para o arquivo
+      $dir = './imagens/'; //Diretório para uploads
+    }
+    
  
     move_uploaded_file($_FILES['pic']['tmp_name'], $dir.$new_name); //Fazer upload do arquivo
     echo '<div class="alert alert-success" role="alert" align="center">
@@ -39,7 +46,7 @@
           <br>
           Imagem enviada com sucesso!
           <br>
-          <a href="exemplo_upload_de_imagens.php">
+          <a href="formulario.php">
           <button class="btn btn-default">Enviar nova imagem</button>
           </a></div>';
  } ?>
