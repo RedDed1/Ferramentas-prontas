a2dd0a5b9c0d0bf8fd6bd0fe49f1d9ba3d7c9026
RedDed1 <jonatha.marvie@gmail.com>
2021-06-11 10:24:01 -0300

    Atualização

--- uploadImagens/formulario.php
+++ uploadImagens/formulario.php
@@ -31,13 +31,8 @@
  {
     $testName = $_POST['imgName'];
     $ext = strtolower(substr($_FILES['pic']['name'],-4)); //Pegando extensão do arquivo
-    if ($testName =! "") {
       $new_name = date("Y.m.d-H.i.s") . $ext; //Definindo um novo nome para o arquivo
       $dir = './imagens/'; //Diretório para uploads
-    } else if ($testName == ""){
-      $new_name = date("Y.m.d-H.i.s") . $ext; //Definindo um novo nome para o arquivo
-      $dir = './imagens/'; //Diretório para uploads
-    }
     
  
     move_uploaded_file($_FILES['pic']['tmp_name'], $dir.$new_name); //Fazer upload do arquivo
